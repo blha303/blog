2277bccb9052d3f0cd5aef037c3c4948287040f6
blha303 <stevensmith.ome@gmail.com>
2015-10-30 05:22:31 +0800

    Added archive view

---
+++ build_archive.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /Users/steven/workspace/blog # to be safe
+rm -rf 2015
+cp _config.yml old_config.yml
+echo "  - jekyll-archives
+
+jekyll-archives:
+  enabled: all
+  permalinks:
+    year: '/:year/'
+    month: '/:year/:month/'
+    day: '/:year/:month/:day/'
+  layouts:
+    year: archive_year
+    month: archive_month
+    day: archive_day
+    tag: archive_tag" >> _config.yml
+
+jekyll build && cp -rf _site/20* .
+mv old_config.yml _config.yml
